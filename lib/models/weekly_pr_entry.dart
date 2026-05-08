6f23d4b8da830651053b4ee058bbe13cbabf8d6b
tarek-kandil <tarekskandil@gmail.com>
2026-05-08 11:44:35 +0200

    feat: add WeeklyPREntry model

---
+++ lib/models/weekly_pr_entry.dart
@@ -0,0 +1,11 @@
+class WeeklyPREntry {
+  final String exerciseName;
+  final double weightKg;
+  final int reps;
+
+  const WeeklyPREntry({
+    required this.exerciseName,
+    required this.weightKg,
+    required this.reps,
+  });
+}
